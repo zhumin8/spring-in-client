5cb0a9ca34e5ee4184284cb389066b74e936020f
Min Zhu <zhumin@google.com>
2024-04-02 17:38:52 -0400

    add sample code -- client lib only.

---
+++ sample-code/src/main/java/com/google/cloud/Main.java
@@ -0,0 +1,50 @@
+package com.google.cloud;
+
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Entity;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
+import java.io.IOException;
+import java.util.List;
+
+public class Main {
+
+  public static void main(String[] args) throws IOException {
+    System.out.println("Hello world!");
+    String[] texts = {
+      "I love this!",
+      "I hate this!",
+      "Google, headquartered in Mountain View (1600 Amphitheatre Pkwy, Mountain View, CA 940430),"
+          + " unveiled the new Android phone for $799 at the Consumer Electronic Show. "
+          + "Sundar Pichai said in his keynote that users love their new Android phones."
+    };
+    // Instantiates a client
+    LanguageServiceClient language = LanguageServiceClient.create();
+
+    for (String text : texts) {
+      Document doc =
+          Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
+      // Detects the sentiment of the text
+      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+
+      System.out.printf("Text: \"%s\"%n", text);
+      System.out.printf(
+          "Sentiment: score = %s, magnitude = %s%n",
+          sentiment.getScore(), sentiment.getMagnitude());
+
+      List<Entity> entities = language.analyzeEntities(doc).getEntitiesList();
+      System.out.println("Analyze Entities: ");
+      entities.forEach(
+          x ->
+              System.out.println(
+                  "Entity: "
+                      + x.getName()
+                      + ", Salience: "
+                      + x.getSalience()
+                      + ", Sentiment: "
+                      + x.getSentiment()
+                      + ", Mention counts: "
+                      + x.getMentionsCount()));
+    }
+  }
+}

2e9a88d251b2ed2915c339d42d93918a2f48dd66
Min Zhu <zhumin@google.com>
2024-04-16 16:52:27 -0400

    wrap client library init in try-with-resources.

--- sample-code-spring-nouse/src/main/java/com/google/cloud/Controller.java
+++ sample-code-spring-nouse/src/main/java/com/google/cloud/Controller.java
@@ -20,7 +20,6 @@ public class Controller {
         + "Sundar Pichai said in his keynote that users love their new Android phones."
   };
 
-
   @GetMapping("/")
   void WelcomePage() {
     System.out.println("Hello world!");
@@ -28,17 +27,21 @@ public class Controller {
 
   @GetMapping("/client")
   void CallWithAutoconfigClient() throws IOException {
-    LanguageServiceClient language = LanguageServiceClient.create();
-    for (String text : this.texts) {
-      com.google.cloud.language.v1.Document doc =
-          com.google.cloud.language.v1.Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
-      // Detects the sentiment of the text
-      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+    try (LanguageServiceClient language = LanguageServiceClient.create()) {
+      for (String text : this.texts) {
+        com.google.cloud.language.v1.Document doc =
+            com.google.cloud.language.v1.Document.newBuilder()
+                .setContent(text)
+                .setType(Document.Type.PLAIN_TEXT)
+                .build();
+        // Detects the sentiment of the text
+        Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
 
-      System.out.printf("Text: \"%s\"%n", text);
-      System.out.printf(
-          "Sentiment: score = %s, magnitude = %s%n",
-          sentiment.getScore(), sentiment.getMagnitude());
+        System.out.printf("Text: \"%s\"%n", text);
+        System.out.printf(
+            "Sentiment: score = %s, magnitude = %s%n",
+            sentiment.getScore(), sentiment.getMagnitude());
+      }
     }
   }
 }
--- sample-code/src/main/java/com/google/cloud/Main.java
+++ sample-code/src/main/java/com/google/cloud/Main.java
@@ -17,19 +17,19 @@ public class Main {
           + "Sundar Pichai said in his keynote that users love their new Android phones."
     };
     // Instantiates a client
-    LanguageServiceClient language = LanguageServiceClient.create();
+    try (LanguageServiceClient language = LanguageServiceClient.create()) {
 
-    for (String text : texts) {
-      Document doc =
-          Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
-      // Detects the sentiment of the text
-      Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
+      for (String text : texts) {
+        Document doc =
+            Document.newBuilder().setContent(text).setType(Document.Type.PLAIN_TEXT).build();
+        // Detects the sentiment of the text
+        Sentiment sentiment = language.analyzeSentiment(doc).getDocumentSentiment();
 
-      System.out.printf("Text: \"%s\"%n", text);
-      System.out.printf(
-          "Sentiment: score = %s, magnitude = %s%n",
-          sentiment.getScore(), sentiment.getMagnitude());
-
+        System.out.printf("Text: \"%s\"%n", text);
+        System.out.printf(
+            "Sentiment: score = %s, magnitude = %s%n",
+            sentiment.getScore(), sentiment.getMagnitude());
+      }
     }
   }
 }
